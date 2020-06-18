d0ec42d67462769f13564224baa4d9d6250129af
PJ Hoberman <pj@twoparts.com>
2020-06-17 21:46:59 -0600

    setting up agent model

--- agents/models.py
+++ agents/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Persona(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Region(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Agent(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    first_time_agent = models.BooleanField()
+    persona = models.ForeignKey(Persona, on_delete=models.SET_NULL, null=True)
+    region = models.ForeignKey(Region, on_delete=models.SET_NULL, null=True)
